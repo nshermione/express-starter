92e46381caa20cb6bf2ced45c12770f3696c93ef
cody <cody@boundlesste.ch>
2023-06-12 23:02:30 +0700

    fix mongo model

--- src/core/Model.mjs
+++ src/core/Model.mjs
@@ -1,28 +1,37 @@
 export class MongoModel {
-  constructor(model) {
-    this.model = model;
+  constructor(collection) {
+    this.collection = collection;
   }
   
   async findOne(...args) {
-    return this.model.findOne(...args);
+    return this.collection.findOne(...args);
   } 
 
   async create(...args) {
-    return this.model.create(...args);
+    return this.collection.create(...args);
   }
 
   async find(...args) {
-    return this.model.findOne(...args);
+    return this.collection.find(...args);
   } 
   
   async update(...args) {
-    return this.model.findOne(...args);
+    return this.collection.update(...args);
   } 
 
   async updateOne(...args) {
-    return this.model.findOne(...args);
+    return this.collection.updateOne(...args);
   } 
+
   async updateMany(...args) {
-    return this.model.updateMany(...args);
+    return this.collection.updateMany(...args);
+  } 
+
+  async deleteOne(...args) {
+    return this.collection.deleteOne(...args);
+  } 
+
+  async deleteMany(...args) {
+    return this.collection.deleteMany(...args);
   } 
 }
